64153b13e7f13e92b4dfa9b45e0c583c4acdc8c6
JMMS14373 <305041439+JMMS14373@users.noreply.github.com>
2026-09-25 16:12:14 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker — Inspeccion de Labores v3.2
 // Optimizado para iOS Safari y Android Chrome
 
-var CACHE_NAME = "inspeccion-labores-v3.7";
+var CACHE_NAME = "inspeccion-labores-v3.10";
 var APP_SHELL = [
   "/",
   "/index.html",
